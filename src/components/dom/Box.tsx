6dd0485ad5e77a01ddf962515a041d1ca736adb4
Ronnie Miksch <ronnie.miksch@clover.com>
2019-02-06 16:28:21 -0800

    some changes

--- src/components/dom/Box.tsx
+++ src/components/dom/Box.tsx
@@ -23,13 +23,14 @@ type Degrees90 = '0deg' | '90deg' | '180deg' | '270deg';
 const rotations: Degrees[] = ['0deg', '45deg', '90deg', '135deg', '180deg', '225deg', '270deg', '315deg'];
 const rotations90: Degrees90[] = ['0deg', '90deg', '180deg', '270deg']; // no multiples of 90
 
+// TODO: fix this to work at any canvas resolution
 const snapToGrid = (value: number): number => Math.round(value / 10) * 10;
 
-const createCSSEditFunc = (el: React.RefObject<HTMLDivElement>) => (attrib: any, value: number, unit: string = 'px') => (el.current!.style[attrib] = value + unit);
+const createCSSEditFunc = (el: React.RefObject<HTMLDivElement>) => (attrib: any, value: any, unit: string = 'px') => (el.current!.style[attrib] = value + unit);
 
-const calculateNewCenterPos = (canvasPos: ClientRect, parentPos: ClientRect) => {
-  const currLeft = parentPos.left - canvasPos.left;
-  const currTop = parentPos.top - canvasPos.top;
+const calculateNewCenterPos = (canvasRect: ClientRect, parentPos: ClientRect) => {
+  const currLeft = parentPos.left - canvasRect.left;
+  const currTop = parentPos.top - canvasRect.top;
 
   const nextLeft = currLeft + parentPos.width / 2 - parentPos.height / 2;
   const nextTop = currTop + parentPos.height / 2 - parentPos.width / 2;
@@ -62,13 +63,13 @@ class Box extends React.Component<TProps, TState> {
     e.stopPropagation();
 
     const { clientX, clientY } = e;
-    const tablePos = this.element.current!.getBoundingClientRect() as DOMRect;
+    const containerPos = this.element.current!.getBoundingClientRect() as DOMRect;
 
     // Save the intial mouse position // TODO:
     this.initialMouseX = clientX;
     this.initialMouseY = clientY;
-    this.initialTableX = tablePos.x;
-    this.initialTableY = tablePos.y;
+    this.initialTableX = containerPos.x;
+    this.initialTableY = containerPos.y;
 
     // Bring the selected element to the foreground
     this.props.reorder(this.props.box.name);
@@ -119,29 +120,29 @@ class Box extends React.Component<TProps, TState> {
     this.finalY = clientY - this.initialMouseY + this.initialTableY - canvasRect.top;
 
     // Reposition the element
-    const tableRect = this.element.current!.getBoundingClientRect() as DOMRect;
+    const containerPos = this.element.current!.getBoundingClientRect() as DOMRect;
 
     const moveTable = createCSSEditFunc(this.element);
 
     // Table pos, and keep sliding when out of bounds
-    if (this.finalX < canvasRect.width - tableRect.width && this.finalY < canvasRect.height - tableRect.height && this.finalX > 0 && this.finalY > 0) {
+    if (this.finalX < canvasRect.width - containerPos.width && this.finalY < canvasRect.height - containerPos.height && this.finalX > 0 && this.finalY > 0) {
       moveTable('top', snapToGrid(this.finalY));
       moveTable('left', snapToGrid(this.finalX));
 
       // Check if the new position won't end up outside the boundaries
-    } else if (this.finalX < canvasRect.width - tableRect.width && this.finalX > 0) {
+    } else if (this.finalX < canvasRect.width - containerPos.width && this.finalX > 0) {
       moveTable('left', snapToGrid(this.finalX));
-    } else if (this.finalY < canvasRect.height - tableRect.height && this.finalY > 0) {
+    } else if (this.finalY < canvasRect.height - containerPos.height && this.finalY > 0) {
       moveTable('top', snapToGrid(this.finalY));
     }
 
     // Mouse pos, sets table to edge if mouse escapes
     if (this.finalX >= canvasRect.width + canvasRect.left) {
-      moveTable('left', snapToGrid(canvasRect.width - tableRect.width));
+      moveTable('left', snapToGrid(canvasRect.width - containerPos.width));
     }
     // TODO: Comments
     if (this.finalY >= canvasRect.height + canvasRect.top) {
-      moveTable('top', snapToGrid(canvasRect.height - tableRect.height));
+      moveTable('top', snapToGrid(canvasRect.height - containerPos.height));
     }
     // TODO: Comments
     if (this.finalX <= 0) {
@@ -168,16 +169,16 @@ class Box extends React.Component<TProps, TState> {
     const { clientX, clientY } = e;
 
     const canvasRect = this.props.floorplan.current!.getBoundingClientRect() as DOMRect;
-    const parentPos = this.element.current!.getBoundingClientRect();
+    const containerPos = this.element.current!.getBoundingClientRect();
 
     const resizeTable = createCSSEditFunc(this.element);
 
     // Check if the new size would grow past the boundaries of the layout
     if (clientX <= canvasRect.width + canvasRect.left) {
       // Check if the new size would be greater than the max
-      if (clientX - parentPos.left <= TABLE_MAX_SIZE[0]) {
+      if (clientX - containerPos.left <= TABLE_MAX_SIZE[0]) {
         // Calculate the new width and height based on the difference between the top/left and the position of the mouse at the bottom/right
-        resizeTable('width', snapToGrid(clientX - parentPos.left));
+        resizeTable('width', snapToGrid(clientX - containerPos.left));
       } else {
         resizeTable('width', TABLE_MAX_SIZE[0]);
       }
@@ -186,20 +187,20 @@ class Box extends React.Component<TProps, TState> {
     // Check if the new size would grow past the boundaries of the layout
     if (clientY <= canvasRect.height + canvasRect.top) {
       // Check if the new size would be greater than the max
-      if (clientY - parentPos.top <= TABLE_MAX_SIZE[1]) {
+      if (clientY - containerPos.top <= TABLE_MAX_SIZE[1]) {
         // Calculate the new width and height based on the difference between the top/left and the position of the mouse at the bottom/right
-        resizeTable('height', snapToGrid(clientY - parentPos.top));
+        resizeTable('height', snapToGrid(clientY - containerPos.top));
       } else {
         resizeTable('height', TABLE_MAX_SIZE[1]);
       }
     }
 
     // Keep the tables above the minimum size
-    if (clientX - parentPos.left <= TABLE_MIN_SIZE[0]) {
+    if (clientX - containerPos.left <= TABLE_MIN_SIZE[0]) {
       resizeTable('width', TABLE_MIN_SIZE[0]);
     }
 
-    if (clientY - parentPos.top <= TABLE_MIN_SIZE[1]) {
+    if (clientY - containerPos.top <= TABLE_MIN_SIZE[1]) {
       resizeTable('height', TABLE_MIN_SIZE[1]);
     }
   };
@@ -208,49 +209,48 @@ class Box extends React.Component<TProps, TState> {
   rotate = () => {
     // Circularly iterate through the list of rotation degrees
     this.rotationIdx = (this.rotationIdx + 1) % rotations.length;
-    this.forceUpdate(); // When commented out, `rotations[this.rotationIdx]` in render is one behind the actual position
+    this.forceUpdate();
     this.shouldRotate = false;
 
-    const parentPos = this.element.current!.getBoundingClientRect();
-    const parentStyle = this.element.current!.style;
-    const tableStyle = this.table.current!.style;
-    const tableDetailsStyle = this.tableDetails.current!.style;
+    const containerPos = this.element.current!.getBoundingClientRect();
     const currentRotation = rotations[this.rotationIdx];
 
-    const moveTable = createCSSEditFunc(this.element);
+    const adjustContainer = createCSSEditFunc(this.element);
+    const adjustTable = createCSSEditFunc(this.table);
+    const adjustTableDetails = createCSSEditFunc(this.tableDetails);
 
     if (rotations90.includes(currentRotation as Degrees90)) {
       // Reset the css transforms because 90 degree rotations are handled by swapping the width and height
-      tableStyle.transform = 'rotate(0deg)';
-      tableDetailsStyle.transform = 'rotate(0deg)';
+      adjustTable('transform', 'rotate(0deg)', '')
+      adjustTableDetails('transform', 'rotate(0deg)', '')
 
-      const canvasPos = this.props.floorplan.current!.getBoundingClientRect() as DOMRect;
+      const canvasRect = this.props.floorplan.current!.getBoundingClientRect() as DOMRect;
 
       // Reverse the width and height
-      parentStyle.width = parentPos.height + 'px';
-      parentStyle.height = parentPos.width + 'px';
+      adjustContainer('width', containerPos.height);
+      adjustContainer('height', containerPos.width);
 
       // Because the rotation is anchored to the top/left, we shift that position to visually maintain the same center point
-      const { nextLeft, nextTop } = calculateNewCenterPos(canvasPos, parentPos);
+      const { nextLeft, nextTop } = calculateNewCenterPos(canvasRect, containerPos);
 
       // Reposition parent element
 
       // TODO: Comment
-      moveTable('top', nextTop);
-      moveTable('left', nextLeft);
-      const newParentPos = this.element.current!.getBoundingClientRect();
+      adjustContainer('top', snapToGrid(nextTop));
+      adjustContainer('left', snapToGrid(nextLeft));
+      const newContainerPos = this.element.current!.getBoundingClientRect();
 
-      if (newParentPos.top + newParentPos.height > canvasPos.height + canvasPos.top) {
-        moveTable('top', canvasPos.height - newParentPos.height);
+      if (newContainerPos.top + newContainerPos.height > canvasRect.height + canvasRect.top) {
+        adjustContainer('top', snapToGrid(canvasRect.height - newContainerPos.height));
       }
-      if (newParentPos.left + newParentPos.width > canvasPos.width + canvasPos.left) {
-        moveTable('left', canvasPos.width - newParentPos.width);
+      if (newContainerPos.left + newContainerPos.width > canvasRect.width + canvasRect.left) {
+        adjustContainer('left', snapToGrid(canvasRect.width - newContainerPos.width));
       }
-      if (newParentPos.top - canvasPos.top < 0) {
-        moveTable('top', 0);
+      if (newContainerPos.top - canvasRect.top < 0) {
+        adjustContainer('top', 0);
       }
-      if (newParentPos.left - canvasPos.left < 0) {
-        moveTable('left', 0);
+      if (newContainerPos.left - canvasRect.left < 0) {
+        adjustContainer('left', 0);
       }
     } else {
       // If you resize a table from a skyscraper shape to a bridge, the rotations need to invert as well.
@@ -263,9 +263,9 @@ class Box extends React.Component<TProps, TState> {
       }
 
       // Rotate the table
-      tableStyle.transform = `rotate(${clockwise ? '' : '-'}${currentRotation})`;
+      adjustTable('transform', `rotate(${clockwise ? '' : '-'}${currentRotation})`, '')
       // The table details need to rotate opposite the table to stay aligned
-      tableDetailsStyle.transform = `rotate(${clockwise ? '-' : ''}${currentRotation})`;
+      adjustTableDetails('transform', `rotate(${clockwise ? '-' : ''}${currentRotation})`, '')
     }
   };
 
